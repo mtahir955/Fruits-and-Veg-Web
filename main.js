02a7d86a3d8c6e89d4fb16bb4d238bd2f252234e
mtahir955 <tahirshabbir955@gmail.com>
2024-08-09 02:54:24 -0700

    Fruits-and-Veg-Web
    Fruits-and-Veg-Web

---
+++ main.js
@@ -0,0 +1,22 @@
+// swiper
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+   
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  let menu = document.querySelector("#menu-icon");
+  let navbar = document.querySelector(".navbar");
+
+  menu.onclick = () => {
+    menu.classList.toggle('bx-x')
+    navbar.classList.toggle('action')
+  }
+  window.onscroll = () => {
+    menu.classList.remove('bx-x')
+    navbar.classList.remove('action')
+  }
